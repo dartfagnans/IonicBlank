a6a84c663c55947e3f78076424a67415fa9e3897
dartfagnans <giuseppe.fagnani@studenti.unicam.it>
2018-05-21 12:37:00 +0200

    aggiunto tasto per fare foto e visualizzarla sulla pagina home

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
+import {Camera, CameraOptions} from '@ionic-native/camera';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { Camera, CameraOptions} from '@ionic-native/camera';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,52 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public base64Image:string = "";
 
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, private camera: Camera) {
+  }
+
+  public showAlert (nome:string) {
+    console.log("qui dovrei mostrare un alert per: " + nome);
+  }
+
+  showConfirm() {
+    let confirm = this.alertCtrl.create({
+      title: 'Use this lightsaber?',
+      message: 'Do you agree to use this lightsaber to do good across the intergalactic galaxy?',
+      buttons: [
+        {
+          text: 'Disagree',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Agree',
+          handler: () => {
+            console.log('Agree clicked');
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+
+  takeShot () {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64:
+     this.base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
   }
 
 }
